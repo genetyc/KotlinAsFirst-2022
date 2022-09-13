8b244107b38712def6e00bbc1eab0eb03cbebeca
Geneticisst <Geneticisst@users.noreply.github.com>
2022-09-13 22:14:43 +0300

    Изменено Lesson2 и Lesson3

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -181,7 +181,7 @@ fun lcm(m: Int, n: Int): Int {
             a > b -> gcd(a % b, b)
             else -> gcd(a, b % a)
         }
-    return (m*n)/gcd(m,n)
+    return (m * n) / gcd(m, n)
 }
 
 /**
@@ -191,7 +191,7 @@ fun lcm(m: Int, n: Int): Int {
  * Взаимно простые числа не имеют общих делителей, кроме 1.
  * Например, 25 и 49 взаимно простые, а 6 и 8 -- нет.
  */
-fun isCoPrime(m: Int, n: Int): Boolean = lcm(m,n) == m*n
+fun isCoPrime(m: Int, n: Int): Boolean = lcm(m, n) == m * n
 
 /**
  * Средняя (3 балла)
